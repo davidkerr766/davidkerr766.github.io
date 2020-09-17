e79619a80278c79b6fc468ebc6ba491a20c81f1b
David Kerr <david.kerr766@outlook.com>
2020-09-18 08:06:38 +1000

    splash page added with scroll function

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,37 @@
-console.log('hello there')
+const smoothScroll = element => {
+    const MIN_PIXELS_PER_STEP = 16;
+    const MAX_SCROLL_STEPS = 30;
+    let target = document.querySelector(element);
+    let scrollContainer = target;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+
+    var targetY = 0;
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+
+    var pixelsPerStep = Math.max(MIN_PIXELS_PER_STEP,
+                                 (targetY - scrollContainer.scrollTop) / MAX_SCROLL_STEPS);
+
+    var stepFunc = function() {
+        scrollContainer.scrollTop =
+            Math.min(targetY, pixelsPerStep + scrollContainer.scrollTop);
+
+        if (scrollContainer.scrollTop >= targetY) {
+            return;
+        }
+
+        window.requestAnimationFrame(stepFunc);
+    };
+
+    window.requestAnimationFrame(stepFunc);
+}
+
+
+const portfolioButton = document.querySelector('#click')
+portfolioButton.addEventListener("click", () => smoothScroll('header'))
